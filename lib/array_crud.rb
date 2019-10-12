34c14be1174bfc62f625293e2405a710652962a7
Tavis Weeden <tavisweeden@gmail.com>
2019-10-12 21:48:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  new_array_2 = [porsche, ferrari, lamborghini, mcclaren]
+  new_array_2 = ["porsche", "ferrari", "lamborghini", "mcclaren"]
 end
 
 def add_element_to_end_of_array(array, element)
